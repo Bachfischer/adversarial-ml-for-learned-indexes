e33e4d81b5d92f029088364a46e28ca6132c1960
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2021-07-16 17:38:55 +0200

    add pure python scripts to perform downsampling and poisoning

---
+++ src/downsample_benchmark_datasets.py
@@ -0,0 +1,17 @@
+import numpy as np
+import struct
+
+def downsample_dataset(dataset_filename : str, downsample_factor):
+    full_keyset = np.fromfile("../data/" + dataset_filename + "_ts_200M_uint64", dtype=np.uint64)[1:]
+    reduced_keyset = np.delete(full_keyset, np.arange(0, full_keyset.size, downsample_factor))
+    downsampled_keyset = full_keyset[::downsample_factor]
+    print("Length of downsampled keyset: ", len(downsampled_keyset))
+    
+    with open("../data/" + dataset_filename + "_ts_1M_uint64", "wb") as output_file:
+        output_file.write(struct.pack("Q", len(downsampled_keyset)))
+        downsampled_keyset.tofile(output_file)
+
+downsample_dataset("wiki", 4096)
+#downsample_dataset("books", 131072)
+#downsample_dataset("osm_cellids", 131072)
+#downsample_dataset("fb", 131072)
---
+++ src/perform_poisoning.py
@@ -0,0 +1,169 @@
+import csv
+import numpy as np
+import pandas as pd
+import operator
+import struct
+
+
+def read_dataset(dataset_filename : str):
+    keyset = np.fromfile("../data/" + dataset_filename, dtype=np.uint64)[1:]
+    print("Length of keyset: ", len(keyset))
+    rankset = rankdata(keyset)
+    keyset = keyset.reshape(-1, 1)
+    return (keyset, rankset)
+
+"""
+Source: Scipy - https://github.com/scipy/scipy/blob/v1.7.0/scipy/stats/stats.py#L8631-L8737
+"""
+def rankdata(array):
+    #arr = np.ravel(np.asarray(array))
+    algo = 'mergesort'
+    sorter = np.argsort(array, kind=algo)
+
+    inv = np.empty(sorter.size, dtype=np.intp)
+    inv[sorter] = np.arange(sorter.size, dtype=np.intp)
+    return inv + 1
+
+# Extracts non-occupied keys for a given sequence
+def partition_non_occupied_keys_all_in_one(K, P):
+    keyset = np.append(K, list(P))
+    keyset = np.sort(keyset)
+    
+    n = keyset.shape[0]
+    
+    lower_bound = int(keyset[0]-1)
+    upper_bound = int(keyset[n-1]+1)
+    
+    # convert to set to speed up lookup
+    keyset = set(keyset)
+    
+    endpoints = []
+    is_in_sequence = False
+    for i in range(lower_bound, upper_bound + 1):
+        # TODO: We limit the number of endpoints to improve performance
+        if len(endpoints) > 1000:
+            return  np.array(endpoints)
+        elif (i not in keyset and is_in_sequence is False): # if key i is at start of sequence
+            #print("Adding " + str(i) + " to non_occupied_keys")
+            is_in_sequence = True
+            endpoints.append(i)
+        elif i not in keyset and is_in_sequence is True and (i+1) in keyset: # if key i is at end of sequence
+            #print("Adding " + str(i) + " to non_occupied_keys because " + str(i+1) + " is not in keyset")
+            endpoints.append(i)
+        else: 
+            is_in_sequence = False
+        
+    return np.array(endpoints)
+
+# Compute the rank that key S(i) would have if it was inserted in K ∪ P and assign this rank as the i-th element of the new sequence
+def compute_rank_for_endpoints(endpoints, keyset):
+    computed_rank_for_endpoint = []
+    
+    for endpoint in endpoints:
+        keyset_extended = np.append(keyset, endpoint)
+        
+        rank = rankdata(keyset_extended)
+        
+        # extract rank for the last element in the list (the endpoint)
+        rank_of_endpoint = rank[len(rank) - 1]
+        computed_rank_for_endpoint.append(int(rank_of_endpoint))
+    
+    return computed_rank_for_endpoint
+
+
+
+
+def obtain_poisoning_keys(p, keyset, rankset):
+    # Total number of elements
+    n = keyset.shape[0]
+
+    # Number of posoning keys P
+    P = int(p * n)
+    poisoning_keys = set()
+    
+
+    for j in range(P):
+        # Partition the non-occupied keys into subsequences such that each subsequence consists of consecutive non-occupied keys;
+        # Extract the endpoints of each subsequence and sort them to construct the new sequence of endpoints S(i), where i <= 2(n + j);
+        
+        # S: endpoints
+        S = partition_non_occupied_keys_all_in_one(keyset, poisoning_keys)
+        #print("Length of endpoints: ", len(S))
+        
+        # TODO: Investigate impact - we downsample the list of endpoints to max n = 1000
+        # Limit number of endpoints to n = 1000
+        S = np.random.choice(S, size = 50)
+
+        # Compute the rank that key S(i) would have if it was inserted in K ∪ P and assign this rank as the i-th element of the new sequence T (i), where i <= 2(n + j) ;
+        # T: list_rank
+        T = compute_rank_for_endpoints(S, keyset)
+
+        # Compute the effect of choosing S(1) as a poisoning key and inserting it to K ∪ P with the appropriate rank adjustments. 
+        # Specifically, evaluate the sequences each of which is the mean M for a different variable, e.g., K, R, KR. Compute MK (1), MK2 (1), MKR(1), and L(1) ;
+
+        
+        delta_S = {}
+        M_K = {}
+        M_K_square = {}
+        M_R = {}
+        M_R_square = {}
+        M_KR = {}
+        L = {}
+        
+        # Calculate M_K(1), M_R(1) etc.
+        # insert first potential poisoning key
+        current_keyset = np.append(keyset, S[0])
+        M_K[0] = np.mean(current_keyset)
+
+        current_rankset = np.append(rankset, T[0])
+        M_R[0] = np.mean(current_rankset)
+
+        M_K_square[0] = np.mean(current_keyset**2)
+
+        M_R_square[0] = np.mean(current_rankset**2)
+
+        M_KR[0] = np.mean(current_keyset*current_rankset)
+
+        nominator = (M_KR[0] - (M_K[0] * M_R[0]))**2
+        denominator = M_K_square[0] - (M_K[0])**2
+        L[0] = - (nominator / denominator) + M_R_square[0] - (M_R[0])**2
+
+        for i in range(1, len(S)-1):
+            # Calculate M_K(i), M_R(i) etc.
+            delta_S[i] = S[i+1] - S[i] 
+
+            M_K[i] = M_K[i-1] + delta_S[i] / (n) 
+            M_K_square[i] = M_K_square[i-1] + (( 2 * S[i] + delta_S[i]) * delta_S[i]) / (n + 1) 
+
+            M_R[i] = (n + 2) / 2
+            M_R_square[i] = ((n+2)*(2*n+3)) / 6
+            M_KR[i] = M_KR[i-1] + ( T[i-1] * delta_S[i]) / (n + 1)
+
+            nominator = (M_KR[i] - M_K[i]*M_R[i])**2
+            denominator = M_K_square[i] - (M_K[i])**2
+            L[i] = - (nominator / denominator) + M_R_square[i] - (M_R[i])**2
+
+        # get argmax of items in L
+        optimal_key_index = max(L.items(), key=operator.itemgetter(1))[0]
+        
+        poisoning_keys.add(S[optimal_key_index])
+    
+    return poisoning_keys
+
+def perform_poisoning(dataset_filename : str, poisoning_percentage):
+    x, y = read_dataset(dataset_filename + "_ts_1M_uint64")
+    poisoning_keys = obtain_poisoning_keys(p = poisoning_percentage, keyset=x, rankset=y)
+
+    x_poisoned = np.append(x, list(poisoning_keys))
+    #y_poisoned = rankdata(x_poisoned)
+    x_poisoned = x_poisoned.reshape(-1,1)
+
+    with open("../data/poisoned_" + dataset_filename + "_ts_1M_uint64_" + str(poisoning_percentage), "wb") as output_file:
+        output_file.write(struct.pack("Q", len(x_poisoned)))
+        x_poisoned.tofile(output_file)
+    print("Wrote poisoned dataset poisoned_" + str(dataset_filename) + "_ts_1M_uint64_" + str(poisoning_percentage) + " to disk")
+
+perform_poisoning("wiki", 0.2)
+#perform_poisoning("books", 0.2)
+#perform_poisoning("osm_cellids", 0.2)
+#perform_poisoning("fb", 0.2)
